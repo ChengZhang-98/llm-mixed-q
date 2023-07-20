178cfd0c2a560d78cfbd1d8fc821003c6a8ee936
Cheng Zhang <chengzhang98@outlook.com>
2023-07-20 09:36:30 +0100

    minor fix

--- experiments/asplos/fine_tune/bert_large_sst2.sh
+++ experiments/asplos/fine_tune/bert_large_sst2.sh
@@ -8,8 +8,8 @@
 #SBATCH --time=24:00:00
 #SBATCH --account=su114-gpu
 #SBATCH --signal=SIGUSR1@90
-#SBATCH --output=log_bert-base-uncased_sst2.txt
-#SBATCH --job-name=bert-base-uncased_sst2
+#SBATCH --output=log_bert-large-uncased_sst2.txt
+#SBATCH --job-name=bert-large-uncased_sst2
 
 module purge
 module load CUDA/11.7.0 GCCcore/11.3.0 GCC/11.3.0 OpenMPI/4.1.4 Python/3.10.4
@@ -22,11 +22,11 @@ quant_config=$work_dir/experiments/asplos/configs/quantize/bypass.toml
 run_dir=$work_dir/experiments/asplos/fine_tune
 cd $run_dir
 
-echo ========== Running BERT Base SST2 ==========
+echo ========== Running BERT large SST2 ==========
 # conda run -n $env_name accelerate launch --multi_gpu fine_tune.py \
 accelerate launch --multi_gpu fine_tune.py \
     --model_arch bert \
-    --model_name bert-base-uncased \
+    --model_name bert-large-uncased \
     --task sst2 \
     --quant_config $quant_config \
     --batch_size_train 16 \
@@ -35,7 +35,7 @@ accelerate launch --multi_gpu fine_tune.py \
     --num_train_epochs 4 \
     --gradient_accumulation_steps 4 \
     --lr_scheduler_type cosine \
-    --output_dir $run_dir/bert_base_sst2 \
-    --project_name bert_base_sst2 \
+    --output_dir $run_dir/bert_large_sst2 \
+    --project_name bert_large_sst2 \
     --with_tracking
 echo ========== Done. ==========

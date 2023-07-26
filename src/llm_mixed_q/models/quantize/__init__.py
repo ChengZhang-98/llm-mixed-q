37ed3bb1c0bd25442aaffe9ab466fc2e4716d189
Cheng Zhang <chengzhang98@outlook.com>
2023-07-26 08:47:00 +0100

    add more quantizers

--- src/llm_mixed_q/models/quantize/__init__.py
+++ src/llm_mixed_q/models/quantize/__init__.py
@@ -1,7 +1,13 @@
 from .quantized_functions import QUANTIZED_FUNC_MAP
 from .quantized_modules import QUANTIZED_MODULE_MAP
+from .quantizers import QUANTIZER_MAP
 from .quant_config_parser import parse_node_config
-from .quantized_layer_profiler import profile_linear_layer, profile_matmul_layer, update_profile
+from .quantized_layer_profiler import (
+    profile_linear_layer,
+    profile_matmul_layer,
+    update_profile,
+)
+
 
 def get_quantized_cls(op: str, config: dict):
     return QUANTIZED_MODULE_MAP[op][config["name"]]
@@ -9,3 +15,7 @@ def get_quantized_cls(op: str, config: dict):
 
 def get_quantized_func(op: str, config: dict):
     return QUANTIZED_FUNC_MAP[op][config["name"]]
+
+
+def get_quantizer(op: str, config: dict):
+    return QUANTIZER_MAP[config["name"]]

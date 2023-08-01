fa9b098d565448aa29b08d30fc559aa23dc41b82
Cheng Zhang <chengzhang98@outlook.com>
2023-08-01 16:50:42 +0100

    add backup script

---
+++ experiments/create_backup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+if [ -z $1 ]; then
+    echo "❗Use timestamp as the backup tag"
+    tag=$(date +%Y-%m-%d_%H-%M-%S)
+else
+    tag=$1
+fi
+
+src_dir=$HOME/Projects/llm-mixed-q/checkpoints
+bk_dir=$HOME/Projects/llm-mixed-q/local_backup && mkdir -p $bk_dir
+bk_zip=$bk_dir/checkpoints_$tag.zip
+
+echo "📂Backup checkpoints to $bk_zip"
+zip -r -1 $bk_zip $src_dir -x *.bin
+echo "✅Done"

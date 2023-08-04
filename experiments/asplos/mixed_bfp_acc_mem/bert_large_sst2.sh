d6f73e818ca48c19da42cbc0050c57ee51eeb476
Cheng Zhang <chengzhang98@outlook.com>
2023-08-04 11:36:53 +0100

    rename , update and move scripts

---
+++ experiments/asplos/mixed_bfp_acc_mem/bert_large_sst2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo "❗Requires <search_tag> as \$1"
+    exit
+fi
+
+if [ -z $2 ]; then
+    echo "❗Requires <search_config> as \$2"
+    exit
+fi
+
+work_dir=$HOME/Projects/llm-mixed-q
+env_name=llm-mixed-q
+run_dir=$work_dir/experiments/asplos/mixed_bfp_acc_mem
+cd $run_dir
+echo ========== Running BERT Large SST2 ==========
+search_tag=$1
+search_config=$2
+
+save_dir=$work_dir/checkpoints/asplos/mixed_bfp_acc_mem/bert_large_sst2/$search_tag && mkdir -p $save_dir
+model_arch=bert
+task=sst2
+ckpt=$work_dir/checkpoints/asplos/fine_tune/bert_large_sst2
+batch_size=128
+num_samples=512
+max_length=196
+
+if [ $USER = "cz98" ]; then
+    conda run -n $env_name python search_cls.py \
+        --model_arch $model_arch \
+        --model_name $ckpt \
+        --task $task \
+        --batch_size $batch_size \
+        --num_samples_per_trial $num_samples \
+        --padding max_length \
+        --max_length $max_length \
+        --save_dir $save_dir \
+        --search_config $search_config
+else
+    python search_cls.py \
+        --model_arch $model_arch \
+        --model_name $ckpt \
+        --task $task \
+        --batch_size $batch_size \
+        --padding max_length \
+        --max_length $max_length \
+        --save_dir $save_dir \
+        --search_config $search_config
+fi
+echo ========== Done. ==========
--- experiments/asplos/mixed_bfp_acc_mem_ops/bert_base_sst2.sh
+++ experiments/asplos/mixed_bfp_acc_mem_ops/bert_base_sst2.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+
 if [ -z $1 ]; then
     echo "❗Requires <search_tag> as \$1"
     exit
 fi
 
+if [ -z $2 ]; then
+    echo "❗Requires <search_config> as \$2"
+    exit
+fi
+
 work_dir=$HOME/Projects/llm-mixed-q
 env_name=llm-mixed-q
-run_dir=$work_dir/experiments/asplos/mixed_int
+run_dir=$work_dir/experiments/asplos/mixed_bfp_acc_mem_ops
 cd $run_dir
-echo ========== Running BERT-large SST2 ==========
+echo ========== Running BERT Base SST2 ==========
 search_tag=$1
 search_config=$2
-save_dir=$work_dir/checkpoints/asplos/mixed_int/bert_large_sst2/$search_tag && mkdir -p $save_dir
+save_dir=$work_dir/checkpoints/asplos/mixed_bfp_acc_mem_ops/bert_base_sst2/$search_tag && mkdir -p $save_dir
 
 model_arch=bert
 task=sst2
-ckpt=$work_dir/checkpoints/asplos/fine_tune/bert_large_sst2
-stat_profile=$work_dir/checkpoints/asplos/profile_statistics/bert_large_sst2/fp32/statistic_profile.toml
+ckpt=$work_dir/checkpoints/asplos/fine_tune/bert_base_sst2
 batch_size=256
 num_samples=512
 max_length=196
@@ -30,6 +35,5 @@ conda run -n $env_name --no-capture-output python search_cls.py \
     --padding max_length \
     --max_length $max_length \
     --save_dir $save_dir \
-    --search_config $search_config \
-    --stat_profile $stat_profile
+    --search_config $search_config
 echo ========== Done. ==========
